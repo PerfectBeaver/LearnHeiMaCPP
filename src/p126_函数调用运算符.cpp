444522925b9032593297d2fcb51f1ac0913cf96f
PerfectBeaver <wendyz7777@gmail.com>
2024-05-27 17:04:08 +0800

    122-130

---
+++ src/p126_函数调用运算符.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class MyPrint
+{
+public:
+    void operator()(string test) {
+        cout << test<< endl;
+    }
+};
+
+void test01() {
+    MyPrint myPrint;
+    myPrint("hello world"); //叫仿函数
+}
+
+class MyAdd
+{
+public:
+    int operator()(int num1, int num2) {
+        return num1+num2;
+    }
+};
+
+void test02() {
+    MyAdd myadd;
+    int ret = myadd(100, 100);
+    cout << "ret = " << ret << endl;
+}
+
+int main() {
+    test02();
+}
---
+++ src/p129_继承对象模型.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+public:
+    int m_A;
+protected:
+    int m_B;
+private:
+    int m_C;
+};
+
+class Son : public Base
+{
+    //父类中所有非静态成员属性都会被子类继承下去
+    //父类的私有成员不能被子类access但是被继承了所以占内存
+public:
+    int m_D;
+};
+
+void test01()
+{
+    cout << "size of Son =" << sizeof(Son) << endl;
+}
+
+int main()
+{
+    test01();
+    return 0;
+}
